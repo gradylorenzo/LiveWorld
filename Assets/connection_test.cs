39d2ef370a915dcdd35fb57d4995f4cec6f68893
gradylorenzo <gradylorenzo@gmail.com>
2015-03-01 01:28:07 -0500

    First Real Commit
    Contains script and test scene to test connection to AWS server.

---
+++ Assets/connection_test.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class connection_test : MonoBehaviour {
+
+    public bool server = false;
+
+    void Start()
+    {
+        if (server)
+        {
+            bool useNat = !Network.HavePublicAddress();
+            Network.InitializeServer(32, 25566, useNat);
+        }
+        else
+        {
+            Network.Connect("52.10.70.115", 25566);
+        }
+    }
+
+    void OnConnectedToServer()
+    {
+        print("connected!");
+    }
+
+    void OnFailedToConnectToServer()
+    {
+        print("failed to connect!");
+    }
+}
