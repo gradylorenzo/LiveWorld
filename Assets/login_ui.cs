91a6a6bcf81621fcdd9298835481f73d1b919706
gradylorenzo <gradylorenzo@gmail.com>
2015-03-02 22:19:42 -0500

    3.2.15 10:19 PM
    Created Login UI and wired to user database

---
+++ Assets/login_ui.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class login_ui : MonoBehaviour {
+
+    public bool loggedIn;
+    public string name;
+    public string pass;
+    public string id;
+
+    public string user_id;
+    public string user_name;
+
+    void OnGUI()
+    {
+        if (!loggedIn)
+        {
+            name = GUI.TextField(new Rect(0, 0, 200, 20), name);
+            pass = GUI.PasswordField(new Rect(0, 25, 200, 20), pass, '*');
+            if (GUI.Button(new Rect(0, 50, 200, 20), "Log In"))
+            {
+                if (name != "" && name != null && pass != "" && pass != null)
+                {
+                    StartCoroutine(doLogin(name, pass));
+                }
+            }
+        }
+        else
+        {
+            GUI.Label(new Rect(0, 0, 300, 20), "Logged in as " + user_name + ", " + user_id);
+        }
+    }
+
+    IEnumerator doLogin(string n, string p)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("playerName", n);
+        form.AddField("playerPass", p);
+        WWW w = new WWW("http://liveworld.byethost22.com/501.php", form);
+        yield return w;
+
+        
+        if (w.text != "")
+        {
+            char[] delim = { '&' };
+            string[] login = w.text.Split(delim);
+            user_id = login[0];
+            user_name = login[1];
+            loggedIn = true;
+            pass = "";
+        }
+        else
+        {
+            print("NLI");
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Required Unity version: 4.6.1 Pro
